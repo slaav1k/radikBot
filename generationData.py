aa9fba814a2d1beac2bfc2648d2d1e5d17b39893
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-09-03 16:17:23 +0300

    Add files via upload

--- generationData.py
+++ generationData.py
@@ -11,34 +11,34 @@ def generate_data():
 
     if is_even_week():
         if datetime.date.today().weekday() == 0:
-            for row in sheet.iter_rows(min_row=2, max_row=5, min_col=5, max_col=6):
+            for row in sheet.iter_rows(min_row=2, max_row=6, min_col=5, max_col=6):
                 for cell in range(0, 2, len(row)):
                     if row[cell + 1].value:
                         t = [row[cell].value, row[cell + 1].value]
                         data.append(t)
         elif datetime.date.today().weekday() == 1:
-            for row in sheet.iter_rows(min_row=6, max_row=9, min_col=5, max_col=6):
+            for row in sheet.iter_rows(min_row=7, max_row=11, min_col=5, max_col=6):
                 for cell in range(0, 2, len(row)):
                     if row[cell + 1].value:
                         t = [row[cell].value, row[cell + 1].value]
                         data.append(t)
         elif datetime.date.today().weekday() == 2:
-            for row in sheet.iter_rows(min_row=10, max_row=13, min_col=5, max_col=6):
+            data.append(["8:10 - 18:40", "Военная подготовка"])
+        elif datetime.date.today().weekday() == 3:
+            for row in sheet.iter_rows(min_row=2, max_row=6, min_col=10, max_col=11):
                 for cell in range(0, 2, len(row)):
                     if row[cell + 1].value:
                         t = [row[cell].value, row[cell + 1].value]
                         data.append(t)
-        elif datetime.date.today().weekday() == 3:
-            data.append(["8:10 - 18:40", "Военная подготовка"])
         elif datetime.date.today().weekday() == 4:
-            for row in sheet.iter_rows(min_row=6, max_row=9, min_col=10, max_col=11):
+            for row in sheet.iter_rows(min_row=7, max_row=11, min_col=10, max_col=11):
                 for cell in range(0, 2, len(row)):
                     if row[cell + 1].value:
                         t = [row[cell].value, row[cell + 1].value]
                         data.append(t)
         elif datetime.date.today().weekday() == 5:
-            for row in sheet.iter_rows(min_row=10,
-                                       max_row=13,
+            for row in sheet.iter_rows(min_row=12,
+                                       max_row=14,
                                        min_col=10,
                                        max_col=11):
                 for cell in range(0, 2, len(row)):
